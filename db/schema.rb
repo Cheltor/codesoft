d39e8a20c5b75b9349b96f8f9eb2cbac5dfa8424
cheltor <rchelton@umd.edu>
2023-02-09 14:16:23 -0500

    nothing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_01_233810) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_09_181510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_01_233810) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "violations", force: :cascade do |t|
+    t.text "description"
+    t.integer "status"
+    t.bigint "address_id", null: false
+    t.datetime "resolved_at"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["address_id"], name: "index_violations_on_address_id"
+    t.index ["user_id"], name: "index_violations_on_user_id"
+  end
+
   add_foreign_key "comments", "addresses"
   add_foreign_key "comments", "users"
+  add_foreign_key "violations", "addresses"
+  add_foreign_key "violations", "users"
 end
