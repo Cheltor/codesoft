7776282861fa21c2faabcaff4f8cceb8e9272bd7
cheltor <rchelton@umd.edu>
2023-03-08 19:46:46 -0500

    dashboard fixes

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,6 @@ class StaticController < ApplicationController
   def dashboard
     @user = current_user
     @recent_comments = @user.comments.order(created_at: :desc).limit(50)
-    @recent_violations = @user.violations.order(created_at: :desc).limit(50)
+    @recent_violations = @user.violations.where(violations: { status: :current }).sort_by(&:deadline_date)
   end
 end
