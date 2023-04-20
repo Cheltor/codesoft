b6ad3cb78ad5552d001c1aace544939b93b27060
cheltor <rchelton@umd.edu>
2023-04-20 10:21:54 -0400

    address model fix

--- app/models/address.rb
+++ app/models/address.rb
@@ -5,4 +5,7 @@ class Address < ApplicationRecord
     def toggle_outstanding_violation
         self.update_attribute(:outstanding_violation, !self.outstanding_violation)
     end      
+    def self.ransackable_attributes(auth_object = nil)
+        ["absent", "combadd", "created_at", "id", "landusecode", "outstanding", "owneraddress", "ownercity", "ownername", "owneroccupiedin", "ownerstate", "ownerzip", "pid", "premisezip", "streetname", "streetnumb", "streettype", "updated_at", "vacant", "zoning"]
+    end
 end
