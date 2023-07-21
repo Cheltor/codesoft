5890fc9c218c8a9225f1a3a22200db6b89fff722
cheltor <rchelton@umd.edu>
2023-07-21 10:54:37 -0400

    adminlte

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,17 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+// AdminKit (required)
+import "./modules/sidebar";
+import "./modules/theme";
+import "./modules/feather";
+
+// Charts
+import "./modules/chartjs";
+
+// Forms
+import "./modules/flatpickr";
+
+// Maps
+import "./modules/vector-maps";
