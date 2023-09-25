fde6be3b770acaaa17b81ab9ba6d9c420228e72a
cheltor <rchelton@umd.edu>
2023-09-25 17:01:22 -0400

    phone numbers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def format_phone_number(phone)
+    phone_number_pattern = /^\(\d{3}\) \d{3}-\d{4}$/
+    
+    if phone.match?(phone_number_pattern)
+      # Phone number is already properly formatted, return as-is
+      phone
+    else
+      # Reformat the phone number to (XXX) XXX-XXXX format
+      formatted_phone = "(#{phone[0..2]}) #{phone[3..5]}-#{phone[6..9]}"
+      formatted_phone
+    end
+  end
 end
