acfcb7b16f2f9ffe6353c932ca243c58e7639edc
cheltor <rchelton@umd.edu>
2024-09-08 19:14:56 -0400

    feat: Add endpoint for retrieving photos of an address

--- config/routes.rb
+++ config/routes.rb
@@ -155,7 +155,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :addresses
+      resources :addresses do
+        member do
+          get 'photos'
+        end
+      end
       resources :businesses
       resources :complaints
       resources :inspections
