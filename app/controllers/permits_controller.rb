396de2bc7de97ede182a5c3610dc44451b7492b7
cheltor <rchelton@umd.edu>
2024-09-30 19:29:36 -0400

    Refactor permit creation to include paid attribute

--- app/controllers/permits_controller.rb
+++ app/controllers/permits_controller.rb
@@ -6,6 +6,51 @@ class PermitsController < ApplicationController
   end
 
   def show
+    @address = @permit.address
+  end
+
+  # Create a method for updating the paid attribute
+  def paid
+    @permit = Permit.find(params[:id])
+    @permit.update_attribute(:paid, !@permit.paid)
+    redirect_to permit_path(@permit)
+  end
+
+  # Create a method for updating the sent attribute
+  def sent
+    @permit = Permit.find(params[:id])
+    @permit.update_attribute(:sent, !@permit.sent)
+    redirect_to permit_path(@permit)
+  end
+
+  # Create a method for updating the revoked attribute
+  def revoked
+    @permit = Permit.find(params[:id])
+    @permit.update_attribute(:revoked, !@permit.revoked)
+    redirect_to permit_path(@permit)
+  end
+
+  # Create a method for updating the permit
+  def update
+    @permit = Permit.find(params[:id])
+    if @permit.update(permit_params)
+      redirect_to permit_path(@permit)
+    else
+      render :edit
+    end
+  end
+
+  # Create a method for editing the permit
+  def edit
+    @permit = Permit.find(params[:id])
+    @address = @permit.address
+  end
+
+  # Create a method for deleting the permit
+  def destroy
+    @permit = Permit.find(params[:id])
+    @permit.destroy
+    redirect_to permits_path
   end
 
   private
--- app/models/permit.rb
+++ app/models/permit.rb
@@ -1,4 +1,40 @@
 class Permit < ApplicationRecord
   belongs_to :address
   belongs_to :inspection
+
+  # When creating a new permit, set the expiration date to the next June 30th
+  before_create do
+    self.expiration_date = Date.new(Date.today.year, 6, 30)
+    self.fiscal_year = Date.today.year
+  end
+
+  # When creating a permit, set the fiscal year. Fiscal years start on July 1st and end on June 30th.
+  before_create do
+    if Date.today.month > 6
+      self.fiscal_year = Date.today.year + 1
+    else
+      self.fiscal_year = Date.today.year
+    end
+  end
+
+  # When creating a permit, set the 'sent' attribute to false
+  before_create do
+    self.sent = false
+  end
+
+  # When creating a permit, set the 'revoked' attribute to false
+  before_create do
+    self.revoked = false
+  end
+
+  # When creating a permit, set the permit number
+  before_create do
+    self.permitnumber = SecureRandom.hex(5)
+  end
+
+  # Whenever a permit is updated, update the inspection's 'paid' attribute
+  after_update do
+    self.inspection.update_attribute(:paid, self.paid)
+  end
+
 end
